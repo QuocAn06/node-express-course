b425e8771c2d4017ed029bc76e0e3c41b2e86911
ale31 <annguyenquoc.le@dxc.com>
2023-10-06 11:24:10 +0700

    Implement App Example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('user hit the resource');
-    res.status(200);
-    res.send('<h1>Home Page</h1>');
+// Setup static and middleware
+app.use(express.static('./public'));
+
+app.get(`/`, (req, res) => {
+    res.sendFile(path.resolve(__dirname, `./navbar-app/index.html`));
 });
 
-app.get('/about', (req, res) => {
-    res.status(200);
-    res.send('<h1>About Page</h1>');
-});
-
-app.all('*', (req, res) => {
-    res.status(404);
-    res.send('<h1>resource not found</h1>');
+app.all(`*`, (req, res) => {
+    res.status(404).send(`resource not found`);
 });
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
+    console.log(`Server is listening on port 5000... `);
 });
