3fe697a9d66c0d08e0256f8c6814b60fac0acf91
ale31 <annguyenquoc.le@dxc.com>
2023-12-23 13:20:24 +0700

    [TMA-0608] - Postman Setup

--- 03-task-manager/routers/tasks.js
+++ 03-task-manager/routers/tasks.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {getAllTasks, createTask, getTask, updateTask, deleteTask} = require('../controllers/tasks');
 
 
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTask);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
