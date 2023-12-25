8e15836f4c03605b7de5c0e9a631bfc620a855b0
ale31 <annguyenquoc.le@dxc.com>
2023-12-25 12:01:20 +0700

    [TMA-0618] - Refactor Connect

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const tasksRouter = require('./routers/tasks');
+const connectDB = require('./db/connect');
 
 //middleware
 app.use(express.json());
@@ -14,4 +15,13 @@ app.use('/api/v1/tasks', tasksRouter);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+const start = async () =>{
+    try {
+        await connectDB();
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
--- 03-task-manager/db/connect.js
+++ 03-task-manager/db/connect.js
@@ -1,1 +1,15 @@
-const connectionString = "mongodb+srv://annguyen:dt8LiHhg2Lqd2k0s@nodeexpressprojects.ckas24k.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb+srv://annguyen:dt8LiHhg2Lqd2k0s@nodeexpressprojects.ckas24k.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
+
+const connectDB = (url) => {
+    return mongoose
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+}
+
+module.exports = connectDB;
