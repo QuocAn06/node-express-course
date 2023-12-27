4107034a9934a8a76ddc8108864183b435e4c3df
ale31 <annguyenquoc.le@dxc.com>
2023-12-27 16:39:49 +0700

    [TMA-06113] - getSingleTask Controller

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -11,15 +11,26 @@ const getAllTasks = async (req, res) => {
 
 const createTask = async (req, res) => {
     try {
-        const task = await Task.create(req.body);
-        res.status(201).json({ task });
+        const response = await Task.create(req.body);
+        res.status(201).json({ response });
     } catch (error) {
         res.status(500).json({ msg: error });
     }
 }
 
-const getTask = (req, res) => {
-    res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const response = await Task.findOne({ _id: taskID });
+
+        if(!response){
+            return res.status(404).json({msg: `No task with id: ${taskID}`});
+        }
+
+        res.status(200).json({ response });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 
 const updateTask = (req, res) => {
