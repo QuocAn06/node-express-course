2d95ba4f05b257943bce2ada39be77f5ce1dfbaa
ale31 <annguyenquoc.le@dxc.com>
2023-12-28 20:40:22 +0700

    update code

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -36,14 +36,20 @@ const deleteTask = asyncWrapper(async (req, res) => {
 
 const updateTask = asyncWrapper(async (req, res) => {
     const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req?.body, {
-        new: true,
-        runValidators: true
-    });
+    const data = req.body;
+    if (data) {
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req?.body, {
+            new: true,
+            runValidators: true
+        });
 
-    if (!task) {
-        return next(createCustomError('No task with id : ' + taskID, 404));
+        if (!task) {
+            return next(createCustomError('No task with id : ' + taskID, 404));
+        }
+    } else {
+        return next(createCustomError('No data in request body', 400));
     }
+
     res.status(200).json({ task })
 });
 
