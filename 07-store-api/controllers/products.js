b78f414b34716184cebae751b25aae4c326e44f9
ale31 <annguyenquoc.le@dxc.com>
2024-01-01 01:40:37 +0700

    [SA-07146] - Numeric Filters - Setup

--- 07-store-api/controllers/products.js
+++ 07-store-api/controllers/products.js
@@ -2,11 +2,9 @@ const { query } = require('express');
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({})
-        .sort('name')
-        .select('name price')
-        .limit(10)
-        .skip(5);
+    const products = await Product.find({ price: { $gt: 30 } })
+        .sort('price')
+        .select('name price');
 
     const result = {
         count: products.length,
